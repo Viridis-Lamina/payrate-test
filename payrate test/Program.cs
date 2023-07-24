540a3d0a28e734972fb096555ace5a7d7bbc5af3
Ryan Clarke <ryanclarke999@gmail.com>
2023-07-24 23:38:47 +0100

    Add project files.

---
+++ payrate test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace payrate 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Please enter your payrate");
+            Console.WriteLine();
+
+            Console.ReadLine();
+
+            double Payrate = double.Parse(Console.ReadLine());
+
+            if (Payrate < 7.50 || Payrate > 49.00) 
+            {
+                Console.WriteLine("Invalid rate");
+
+            
+            }
+            else
+            {
+                Console.WriteLine("The rate is Valid");
+            }
+
+
+
+        }
+    }
+}
